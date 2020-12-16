e5c006ac05f782a229a0578569bd536f532b3ab9
Viktor Soshko <Lorgar4@ukr.net>
2020-12-16 19:27:25 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,26 @@
+/* eslint-disable no-undef */
 import {ExcelComponent} from '../../core/ExcelComponent';
+import {shouldResize} from './table.functions';
+import {resizeHandler} from './table.resize';
 import {createTable} from './table.template.js';
+
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable();
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+
+import {$} from '../../core/dom';
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target);
+  const $parent = $resizer.closest('[data-type="resizable"]');
+  const coords = $parent.getCoords();
+  const resizeType = $resizer.data.resize;
+  const resizeElement = $parent.data[resizeType];
+  const sideProp = resizeType === 'col' ? 'bottom' : 'right';
+  let value;
+
+  $resizer.css({opacity: 1,
+    [sideProp]: '-5000px'});
+
+  document.onmousemove = (e) => {
+    let delta;
+    if (resizeType === 'col') {
+      delta = e.pageX - coords.right;
+
+      value = coords.width + delta + 'px';
+      $resizer.css({right: -delta + 'px'});
+    } else {
+      delta = e.pageY - coords.bottom;
+      value = coords.height + delta + 'px';
+
+      $resizer.css({bottom: -delta + 'px'});
+    }
+  };
+
+  document.onmouseup = () => {
+    document.onmousemove = null;
+    document.onmouseup = null;
+    $resizer.css({opacity: '',
+      bottom: 0,
+      right: 0});
+
+    if (resizeType === 'col') {
+      $parent.css({width: value});
+      $root
+          .findAll(`[data-col="${resizeElement}"]`)
+          .forEach((el) => el.style.width = value);
+    } else {
+      $parent.css({height: value});
+    }
+  };
+}
